e77e569c0d6761dd0302cfbc3ba8858e4fe682db
Chichetin <alecksandrchicherin@gmail.com>
2026-01-16 14:38:30 +0300

    Создана система аутентификации

--- app/models.py
+++ app/models.py
@@ -1,15 +1,26 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+
+class User(UserMixin, db.Model): # Наследуем UserMixin
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
-    # Связь с книгами: один пользователь может иметь много книг
-    books = db.relationship('Book', backref='owner', lazy='dynamic', cascade="all, delete-orphan")
+    password_hash = db.Column(db.String(128)) # Меняем password на хеш
+    books = db.relationship('Book', backref='owner', lazy='dynamic')
 
-    def __repr__(self):
-        return f'<User {self.username}>'
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# Функция для загрузки пользователя по ID из сессии
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Book(db.Model):
     __tablename__ = 'books'
